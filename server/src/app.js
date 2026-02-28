64a77dd7889326749a540fa2b357befc79f7f967
Pratikpadole-07 <padolepratik25@gmail.com>
2026-02-28 23:30:12 +0530

    Initial backend setup with authentication and JWT middleware

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
+import protect from "./middleware/auth.middleware.js";
 
 const app=express();
 app.use(cors());
@@ -9,4 +10,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
+app.get("/api/test", protect, (req, res) => {
+  res.json({ message: "Protected route working", user: req.user });
+});
 export default app;
