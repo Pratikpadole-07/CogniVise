0c9f6aa2f4e21c9dee1e72fb4bbdb9302a8dc883
Pratikpadole-07 <padolepratik25@gmail.com>
2026-04-25 13:01:54 +0530

    main.jsx added

---
+++ client/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
+import { AuthProvider } from './context/AuthContext.jsx'
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  
+)
+
