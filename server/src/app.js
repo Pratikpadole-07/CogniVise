3b9d2a74ec73f14c414d42a72fd9d0b37adffdcb
Pratikpadole-07 <padolepratik25@gmail.com>
2026-04-25 13:31:54 +0530

    port update

--- server/src/app.js
+++ server/src/app.js
@@ -7,12 +7,22 @@ import studyLogRoutes from "./routes/studyLog.routes.js";
 import topicRoutes from "./routes/topic.routes.js";
 import studyPlanRoutes from "./routes/studyPlan.routes.js";
 import analyticsRoutes from "./routes/analytics.routes.js";
-
+import masteryRoutes from "./routes/mastery.routes.js";
 
 
 
 const app=express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+    "http://localhost:5173",
+    "http://localhost:5174"
+  ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -20,6 +30,7 @@ app.use("/api/study-logs", studyLogRoutes);
 app.use("/api/topics", topicRoutes);
 app.use("/api/study-plans", studyPlanRoutes);
 app.use("/api/analytics", analyticsRoutes);
+app.use("/api/mastery", masteryRoutes);
 
 app.get("/api/test", protect, (req, res) => {
   res.json({ message: "Protected route working", user: req.user });
